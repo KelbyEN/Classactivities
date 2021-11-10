11c473f7a270a3fa1f854cf0ad294b5b041bd8b3
Kelby Estes-Nelson <kelbyalese417@gmail.com>
2021-11-10 12:47:07 -0600

    2.2.1--JSVariablesAndPrimitives.js

---
+++ 2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,42 @@
+// create a variable called "name" that references a string
+let name = "Kelby";
+// create a variable called "favoriteSong" that references a string
+let song = "Good Days";
+
+// create a variable called "wage" that references a number
+let wage = 21;
+// create a variable called "age" that references a number
+let age = 35;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
+
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+
+// create a variable called "favoriteThing" that references ANY primitive value
+let favoriteThing = "poetry";
+
+// print the data type of "favoriteThing"
+console.log(typeOf poetry);
+
+// ! You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteTHing = "songs";
+// ! You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = 23;
+
+// ! Happy Birthday!
+// Reassign "age" to a new value
+age= 53;
+
+
+// ! You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 40;
+// ! Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+let satisfied = "true";
+
+// use "console.log()" and "+" to print a sentence that includes two variables
+console.log("my name is" + name + "i am" + age + ".");
